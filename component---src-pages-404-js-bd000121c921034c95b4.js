29c8cf4dd6382e03ea194738bbe265d20e384c51
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-14 20:18:16 +0000

    Deploy lourenci/react-kanban to github.com/lourenci/react-kanban.git:gh-pages

---
+++ component---src-pages-404-js-bd000121c921034c95b4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(t,e,n){"use strict";n.r(e);n("mXGw");var i=n("aD51");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-bd000121c921034c95b4.js.map
---
+++ styles-957beefc3b3edfc1c11f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{oKgo:function(o,n,w){}}]);
+//# sourceMappingURL=styles-957beefc3b3edfc1c11f.js.map
